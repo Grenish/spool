54cdf82d71fa41d77e67d892d08c6872540e4729
Grenish rai <mrcoder2033d@gmail.com>
2026-04-18 06:48:15 +0530

    Create editor-body.tsx

---
+++ components/dashboard/editor/editor-body.tsx
@@ -0,0 +1,18 @@
+import { CardContent } from "@/components/ui/card";
+
+export default function EditorBody() {
+  return (
+    <CardContent className="border">
+      <h2 className="text-2xl font-bold mb-2">Hello World</h2>
+      <p className="mb-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </CardContent>
+  );
+}
